b0c926c51f36929f81e27359d2da1d42cd7d6e2a
goat-debug <mhamini380@gmail.com>
2025-05-09 01:04:47 +0330

    Restore: SQL queries file with currency updated to toman

---
+++ ShopProductManagerSQLQueries.sql
@@ -0,0 +1,19 @@
+﻿
+Microsoft Visual Studio Solution File, Format Version 12.00
+# SQL Server Management Studio Solution File, Format Version 20.00
+VisualStudioVersion = 15.0.28307.421
+MinimumVisualStudioVersion = 10.0.40219.1
+Global
+	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+		Default|Default = Default|Default
+	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+		{792D8AFF-AD8F-4868-84A1-58359C431307}.Default|Default.ActiveCfg = Default
+	EndGlobalSection
+	GlobalSection(SolutionProperties) = preSolution
+		HideSolutionNode = FALSE
+	EndGlobalSection
+	GlobalSection(ExtensibilityGlobals) = postSolution
+		SolutionGuid = {DDFA39C3-E9FA-43E3-B809-BD0966F54C93}
+	EndGlobalSection
+EndGlobal
